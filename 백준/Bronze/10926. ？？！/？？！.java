ae591fe4ef7ef096f5073231108352c61acab50b
yuseung <48000689+yu5-win@users.noreply.github.com>
2023-09-28 10:21:37 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        String name = sc.next();
+        
+        System.out.println(name+"??!");
+    }
+}
