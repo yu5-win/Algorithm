c244114e796a5bcf99ef3d100027d2a87ee75a4d
yuseung <48000689+yu5-win@users.noreply.github.com>
2024-02-03 23:54:53 +0900

    [Bronze III] Title: 최댓값, Time: 128 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,27 +1,32 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int[] arr = new int[9];
-
-        for (int i = 0; i < 9; i++) {
-            arr[i] = sc.nextInt();
-        }
-        sc.close();
-
-        int max = 0;
-        int count = 0;
-        int index = 0;
-
-        for (int bi : arr) {
-            count++;
-            if (bi > max) {
-                max = bi;
-                index = count;
-            }
-        }
-        System.out.println(max);
-        System.out.println(index);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int qt = 9;
+		int num = Integer.MIN_VALUE;
+		int idx = -1;
+		
+		List<Integer> arr = new ArrayList<>();
+		for(int i = 1; i <= qt; i++) {
+			int a = Integer.parseInt(br.readLine());
+			arr.add(a);
+		}
+		
+		for(int i = 0; i < arr.size(); i++) {
+			if(arr.get(i) > num) {
+				num = arr.get(i);
+				idx = i + 1;
+			}
+		}
+		
+		System.out.println(num);
+		System.out.println(idx);
+	}
 }
