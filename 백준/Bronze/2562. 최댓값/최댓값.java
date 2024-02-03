860fea30495366100bd15b6395fdee1a1c864bc6
yuseung <48000689+yu5-win@users.noreply.github.com>
2024-02-03 23:49:00 +0900

    [Bronze III] Title: 최댓값, Time: 204 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[9];
+
+        for (int i = 0; i < 9; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+
+        int max = 0;
+        int count = 0;
+        int index = 0;
+
+        for (int bi : arr) {
+            count++;
+            if (bi > max) {
+                max = bi;
+                index = count;
+            }
+        }
+        System.out.println(max);
+        System.out.println(index);
+    }
+}
