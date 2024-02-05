2f1d00de8f94c4e2df1c92be4cdeaae477823334
yuseung <48000689+yu5-win@users.noreply.github.com>
2024-02-05 22:52:11 +0900

    [Bronze V] Title: 팩토리얼, Time: 120 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int a = Integer.parseInt(br.readLine());
+		
+		int z = 1;
+		for(int i = 1; i <= a; i++) {
+			z = z * i;
+		}
+		System.out.print(z);
+	}
+}
