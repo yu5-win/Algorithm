e68b591daed2cbe9d1941077968d61a1a8b468d4
yuseung <48000689+yu5-win@users.noreply.github.com>
2024-02-05 22:47:44 +0900

    [Bronze V] Title: N 찍기, Time: 1024 ms, Memory: 31204 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int a = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < a; i++) {
+			System.out.println(i+1);	
+		}
+	}
+}
